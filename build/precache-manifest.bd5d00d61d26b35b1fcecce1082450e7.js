b295482c2a8ab927ee03a4f6cf7e6c81a9e9c671
tvpeter <withtvpeter@gmail.com>
2019-09-25 15:50:17 +0100

    complete user interface for the app

--- build/precache-manifest.bd5d00d61d26b35b1fcecce1082450e7.js
+++ build/precache-manifest.bd5d00d61d26b35b1fcecce1082450e7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b06aae9a56d6327b6818c66f53ac9105",
+    "revision": "cb888e5c8f72ed18a30e3d5a4bfd8f23",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "a3a7b999f88deeb14db8",
-    "url": "/static/css/main.ed29a692.chunk.css"
+    "revision": "2cecfb71a43634af3a9d",
+    "url": "/static/css/main.15e28cc4.chunk.css"
   },
   {
     "revision": "340dbc142fa20807363a",
     "url": "/static/js/2.003d731f.chunk.js"
   },
   {
-    "revision": "a3a7b999f88deeb14db8",
-    "url": "/static/js/main.c5ea83d1.chunk.js"
+    "revision": "2cecfb71a43634af3a9d",
+    "url": "/static/js/main.281588d2.chunk.js"
   },
   {
     "revision": "0eccdfee5fbb90f5b7ec",
